dae647d1388da8f3584effc729f7ce7f030c85fd
Tanmay Goyal <tanmay15106@iiitd.ac.in>
2018-05-29 18:08:39 +0530

    testing

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,17 @@
 
 
 
+# cd bin/;
+# ls;
+
 if [ -f tests/MavenTests/test.xml ]; then
 	rm test*.xml
 fi
 
 
 if [ -f ./bin/MavenTests ]; then
+	echo "inside if 2"
+
 	./bin/MavenTests -xml
 
     MY_VAR=$?
